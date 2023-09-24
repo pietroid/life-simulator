9aa53edabfac767c97d59d322bcb942f34afadac
^Pietro <^pietrodomingues@gmail.com^>
2023-09-24 15:41:40 -0300

    adding basic structure for live cells

---
+++ cells/README.md
@@ -0,0 +1,23 @@
+## Cells simulation
+
+Cell here considered the most basic element of life.
+
+They contain as internal information:
+
+- Genetic code
+- Level of energy
+- Size
+- Age
+- Level of life (0 being dead, 1 alive)
+
+They can sense externally:
+
+- Varied chemical receptors gradients
+
+They can act:
+
+- Diving theirselves
+- Eating something
+- Moving in some direction (vx and vy)
+- Dying (self-destroying)
+- Sending varied chemical signals
---
+++ cells/main.py
@@ -0,0 +1,24 @@
+class Cell:
+    def __init__(self, x, y, code, energy, size, age, life_level):
+        self.x = x
+        self.y = y
+        self.code = code
+        self.energy = energy
+        self.size = size
+        self.age = age
+        self.life_level = life_level
+        
+        self.division = False
+        self.vx = 0
+        self.vy = 0
+
+    def iterate(self):
+        x += vx
+        y += vy
+
+        act(self, signals):
+            pass
+
+    
+
+    
